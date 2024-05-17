58979e400514d0f853369de6de4fd0fd8de21aff
HolbyKate <6804@holbertonstudents.com>
2024-05-17 19:47:44 +0000

    intent insert a number into a sorted list

---
+++ insert_in_sorted_linked_list/0-insert_number.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+
+listint_t *insert_node(listint_t **head, int number)
+{
+    listint_t *new_node, *current, *prev;
+
+    new_node = malloc(sizeof(listint_t));
+    if (new_node == NULL)
+        return (NULL);
+
+    new_node->n = number;
+    new_node->next = NULL;
+
+    if (*head == NULL || number < (*head)->n)
+    {
+        new_node->next = *head;
+        *head = new_node;
+        return (new_node);
+    }
+
+    while (current != NULL && number > current->n)
+    {
+        prev = current;
+        current = current->next;
+    }
+
+    prev->next = new_node;
+    new_node->next = current;
+
+    return (new_node);
+}

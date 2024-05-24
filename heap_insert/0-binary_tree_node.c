4f984de46d5a26d41444538fafbd42c9ee3c3676
HolbyKate <6804@holbertonstudents.com>
2024-05-24 15:24:17 +0000

    Update all file because pb of compilation

--- heap_insert/0-binary_tree_node.c
+++ heap_insert/0-binary_tree_node.c
@@ -2,25 +2,23 @@
 #include "binary_trees.h"
 
 /**
-* binary_tree_node - Creates a binary tree node
-* @parent: pointer to the parent node of the node to create
-* @value: value to put in the new node
-*
-* Return: Pointer to the new node, or NULL on failure
-*/
-
+ * binary_tree_node - Creates a binary tree node.
+ * @parent: Pointer to the parent node.
+ * @value: Value of the node.
+ *
+ * Return: A pointer to the newly created node, or NULL on failure.
+ */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node;
+	binary_tree_t *newNode = malloc(sizeof(binary_tree_t));
 
-    new_node = malloc(sizeof(binary_tree_t));
-    if (new_node == NULL)
-        return (NULL);
+	if (newNode == NULL)
+		return (NULL);
 
-    new_node->n = value;
-    new_node->parent = parent;
-    new_node->left = NULL;
-    new_node->right = NULL;
+	newNode->parent = parent;
+	newNode->n = value;
+	newNode->left = NULL;
+	newNode->right = NULL;
 
-    return (new_node);
+	return (newNode);
 }
--- heap_insert/binary_trees.h
+++ heap_insert/binary_trees.h
@@ -1,6 +1,9 @@
 #ifndef _BINARY_TREES_H_
 #define _BINARY_TREES_H_
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <stddef.h>
 
 /**
@@ -11,14 +14,25 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-typedef struct binary_tree_s
+struct binary_tree_s
 {
-	int n;
-	struct binary_tree_s *parent;
-	struct binary_tree_s *left;
-	struct binary_tree_s *right;
-} binary_tree_t;
+    int n;
+    struct binary_tree_s *parent;
+    struct binary_tree_s *left;
+    struct binary_tree_s *right;
+};
 
-void binary_tree_print(const binary_tree_t *);
+typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s heap_t;
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+void binary_tree_print(const binary_tree_t *tree);
+int main(void);
+heap_t *heap_insert(heap_t **root, int value);
+size_t binary_tree_size(const binary_tree_t *tree);
+void swap(heap_t **arg_node, heap_t **arg_child);
+int btree_is_perfect(const binary_tree_t *tree);
+int height(const binary_tree_t *tree);
+void update_child_and_node_parents(heap_t *child, heap_t *node, int left_right, heap_t *parent);
 
 #endif /* _BINARY_TREES_H_ */

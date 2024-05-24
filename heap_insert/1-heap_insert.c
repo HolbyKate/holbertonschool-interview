59d6ccfa22b12c672d91a8ba910b4268011effc6
HolbyKate <6804@holbertonstudents.com>
2024-05-24 21:59:46 +0000

    Betty style

--- heap_insert/1-heap_insert.c
+++ heap_insert/1-heap_insert.c
@@ -1,11 +1,11 @@
 #include "binary_trees.h"
 
 /**
- * height - measures the height of a tree
- *
- * @tree: tree root
- * Return: height
- */
+* height - measures the height of a tree
+*
+* @tree: tree root
+* Return: height
+*/
 int height(const binary_tree_t *tree)
 {
     int left = 0;
@@ -23,11 +23,11 @@ int height(const binary_tree_t *tree)
 }
 
 /**
- * btree_is_perfect - checks if a binary tree is perfect
- *
- * @tree: tree root
- * Return: 1 if tree is perfect, 0 otherwise
- */
+* btree_is_perfect - checks if a binary tree is perfect
+*
+* @tree: tree root
+* Return: 1 if tree is perfect, 0 otherwise
+*/
 int btree_is_perfect(const binary_tree_t *tree)
 {
     _Bool left_is_leaf;
@@ -58,12 +58,12 @@ int btree_is_perfect(const binary_tree_t *tree)
 }
 
 /**
- * swap - swaps nodes when child is greater than parent
- *
- * @arg_node: parent node
- * @arg_child: child node
- * Return: no return
- */
+* swap - swaps nodes when child is greater than parent
+*
+* @arg_node: parent node
+* @arg_child: child node
+* Return: no return
+*/
 void swap(heap_t **arg_node, heap_t **arg_child)
 {
     heap_t *node, *child, *node_child, *node_left, *node_right, *parent;
@@ -109,11 +109,11 @@ void swap(heap_t **arg_node, heap_t **arg_child)
 }
 
 /**
- * heap_insert - function that inserts a value in Max Binary Heap
- * @root: double pointer to the root of the heap
- * @value: value to be inserted
- * Return: pointer to the created node, or NULL on failure.
- */
+* heap_insert - function that inserts a value in Max Binary Heap
+* @root: double pointer to the root of the heap
+* @value: value to be inserted
+* Return: pointer to the created node, or NULL on failure.
+*/
 heap_t *heap_insert(heap_t **root, int value)
 {
     heap_t *new;

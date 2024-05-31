a2b0817136da4bd0fc4ed4d4cda1dae0a1070245
HolbyKate <6804@holbertonstudents.com>
2024-05-31 14:47:09 +0000

    Fix picodestyle

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -2,13 +2,16 @@
 """Module that calculate the fewest number of operations"""
 
 def minOperations(n):
-    """Calculate the minimum number of operations required to obtain n from 1"""
+    """
+    Calculate the minimum number of operations required to obtain n from 1
+    """
+    
     if n <= 1:
         return 0
 
     operations = 0
     factorization = 2
-    
+
     while n > 1:
         """While n divisible by factorization"""
         while n % factorization == 0:

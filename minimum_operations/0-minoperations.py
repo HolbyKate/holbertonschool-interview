2abb4d17074698d7d9fbcd234bb4f3acbad4fae9
HolbyKate <6804@holbertonstudents.com>
2024-05-31 14:42:49 +0000

    Fix : order operations and factorizations.

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-"""Method that calculate the fewest n umber of operations"""
+"""Method that calculate the fewest number of operations"""
 
 def minOperations(n):
     """initialize a variable to count the operations"""
     if n <= 1:
         return 0
 
-    operations = 2
-    factorization = 0
-
+    operations = 0
+    factorization = 2
+"""While n divisible by factorization"""
     while n > 1:
-        while n % operations == 0:
-            factorization += operations
-            n // = operations
-        operations += 1
+        while n % factorization == 0:
+            operations += factorization
+            n //= factorization
+        factorization += 1
 
     return operations

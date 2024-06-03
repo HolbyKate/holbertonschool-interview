1bd5b5eea582e299436f8b6350c9d016e6e43f15
HolbyKate <6804@holbertonstudents.com>
2024-06-03 13:37:05 +0000

    First attempt for computes sum of two sandpiles

---
+++ sandpiles/0-sandpiles.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "sandpiles.h"
+
+/**
+ * sandpiles_sum - Computes sum of two sandpiles
+ * @grid1: Left 3x3 grid
+ * @grid2: Right 3x3 grid
+ *
+ */
+
+
+void sandpiles_sum(int grid1[3][3], int grid2[3][3]);

d1a95587c27a4eebe5260d1cc9b30ad56ec78d59
Cathy Augustin <135758000+HolbyKate@users.noreply.github.com>
2024-06-05 16:49:59 +0200

    First print for grid1

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -12,3 +12,21 @@
 
 
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+{
+    int i, j;
+    /*Compute the sum of the 2 sandpiles*/
+
+    for (i = 0; i <3; i++)
+    {
+        for (j = 0; j <3; j++)
+        {
+            grid1[i][j] +=grid2[i][j];
+        }
+    }
+    /*Loop until sandpiles is stable*/
+    while (is_unstable(grid1))
+    {
+        print_grid(grid1);
+        toppling(grid1);
+    }
+}

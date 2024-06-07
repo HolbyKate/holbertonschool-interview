02b32787547f59f9aff3c18496905aa223a957d4
HolbyKate <cathy.augustin@holbertonstudents.com>
2024-06-07 14:18:43 +0200

    Testing again

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -1,119 +1,103 @@
 #include "sandpiles.h"
+#include <stdio.h>
 
 /**
- * grid_addition - ajoute 2 grilles
- * @grid1: première matrice
- * @grid2: deuxième matrice
+ * print_grid - prints a grid
+ * @grid: grid to print
  */
-
-void grid_addition(int grid1[3][3], int grid2[3][3])
+static void print_grid(int grid[3][3])
 {
-	int i, j;
+    int i, j;
 
-	for (i = 0; i < 3; i++)
-	{
-		for (j = 0; j < 3; j++)
-		{
-			grid1[i][j] = grid1[i][j] + grid2[i][j];
-		}
-	}
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            if (j)
+                printf(" ");
+            printf("%d", grid[i][j]);
+        }
+        printf("\n");
+    }
 }
 
 /**
- * grid_print - affiche une grille
- * @grid: matrice
+ * add_grids - sums two sandpiles
+ * @grid1: first sandpile
+ * @grid2: second sandpile
  */
-
-void grid_print(int grid[3][3])
+void add_grids(int grid1[3][3], int grid2[3][3])
 {
-	int i = 0;
-	int j = 0;
+    size_t i, j;
 
-	printf("=\n");
-	for (i = 0; i < 3; i++)
-	{
-		for (j = 0; j < 3; j++)
-		{
-			if (j)
-				printf(" ");
-			printf("%d", grid[i][j]);
-		}
-		printf("\n");
-	}
+    for (i = 0; i < 3; ++i)
+        for (j = 0; j < 3; ++j)
+            grid1[i][j] += grid2[i][j];
 }
 
 /**
- * grid_is_ok - vérifie si une grille est correcte
- * @grid: matrice
- * Return: 1 si c'est correct, 0 sinon
+ * is_stable - checks if a sandpile is stable
+ * @grid: sandpile to check
+ *
+ * Return: 1 if stable, 0 otherwise
  */
-
-int grid_is_ok(int grid[3][3])
+int is_stable(int grid[3][3])
 {
-	int i = 0;
-	int j = 0;
+    size_t i, j;
 
-	for (i = 0; i < 3; i++)
-	{
-		for (j = 0; j < 3; j++)
-		{
-			if (grid[i][j] > 3) /* si une cellule est supérieure à 3 */
-				return (0); /* la grille n'est pas ok */
-		}
-	}
-	return (1); /* la grille est ok */
+    for (i = 0; i < 3; ++i)
+        for (j = 0; j < 3; ++j)
+            if (grid[i][j] > 3)
+                return (0);
+    return (1);
 }
 
 /**
- * grid_change - change la valeur d'une grille
- * @grid1: matrice
+ * topple - topples an unstable sandpile
+ * @grid: sandpile to topple
  */
-
-void grid_change(int grid1[3][3])
+void topple(int grid[3][3])
 {
-	int i = 0;
-	int j = 0;
-	int gridx[3][3];
+    int i, j;
+    int temp_grid[3][3] = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
 
-	for (i = 0; i < 3; i++)
-	{
-		for (j = 0; j < 3; j++)
-			gridx[i][j] = 0; /* initialise gridx à 0 */
-	}
+    for (i = 0; i < 3; ++i)
+    {
+        for (j = 0; j < 3; ++j)
+        {
+            if (grid[i][j] > 3)
+            {
+                temp_grid[i][j] -= 4;
+                if (i - 1 >= 0)
+                    temp_grid[i - 1][j]++;
+                if (i + 1 < 3)
+                    temp_grid[i + 1][j]++;
+                if (j - 1 >= 0)
+                    temp_grid[i][j - 1]++;
+                if (j + 1 < 3)
+                    temp_grid[i][j + 1]++;
+            }
+        }
+    }
 
-	for (i = 0; i < 3; i++) /* parcourt les lignes de grid1 */
-	{
-		for (j = 0; j < 3; j++) /* parcourt les colonnes de grid1 */
-		{
-			if (grid1[i][j] > 3) /* si une cellule est supérieure à 3 */
-			{
-				grid1[i][j] = grid1[i][j] - 4; /* soustrait 4 à la cellule */
-				if ((i - 1) >= 0 && (i - 1) < 3) /*si une cellule est dans la grille*/
-					gridx[i - 1][j] += 1; /* ajoute 1 à la cellule au-dessus */
-				if ((j - 1) >= 0 && (j - 1) < 3)
-					gridx[i][j - 1] += 1; /* ajoute 1 à la cellule à gauche */
-				if ((i + 1) >= 0 && (i + 1) < 3)
-					gridx[i + 1][j] += 1; /* ajoute 1 à la cellule en dessous */
-				if ((j + 1) >= 0 && (j + 1) < 3)
-					gridx[i][j + 1] += 1; /* ajoute 1 à la cellule à droite */
-			}
-		}
-	}
-	grid_addition(grid1, gridx); /* grid1 = grid1 + gridx */
+    for (i = 0; i < 3; ++i)
+        for (j = 0; j < 3; ++j)
+            grid[i][j] += temp_grid[i][j];
 }
 
 /**
- * sandpiles_sum - additionne 2 tas de sable
- * @grid1: première matrice
- * @grid2: deuxième matrice
+ * sandpiles_sum - computes the sum of two sandpiles
+ * @grid1: first sandpile
+ * @grid2: second sandpile
  */
-
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
-	grid_addition(grid1, grid2); /* grid1 = grid1 + grid2 */
-	while (!grid_is_ok(grid1)) /* tant que grid1 n'est pas correcte */
-	{
-		grid_print(grid1);
-		grid_change(grid1);
-	}
+    add_grids(grid1, grid2);
+
+    while (!is_stable(grid1))
+    {
+        puts("=");
+        print_grid(grid1);
+        topple(grid1);
+    }
 }

65f7960c769f587a1d6cafffb9b426bb6a009752
HolbyKate <cathy.augustin@holbertonstudents.com>
2024-07-23 00:07:00 +0200

    Fix: description of if empty

--- menger/0-menger.c
+++ menger/0-menger.c
@@ -18,8 +18,8 @@ int is_empty(int row, int column)
 {
     /*Check all level*/
 	while (row > 0 || column > 0)
-		/*Check if he current position at any level is in the middle third*/
 	{
+        /*Check if he current position at any level is in the middle third*/
 		if (row % 3 == 1 && column % 3 == 1)
 			return (1);
 		/*Move to the next level*/
@@ -33,9 +33,10 @@ int is_empty(int row, int column)
  * menger - Draws a 2D Menger Sponge
  * @level: The level of the Menger Sponge to draw
  *
- * This function prints a 2D representation of a Menger Sponge of a given level.
+ * This function prints a 2D representation of a Menger Sponge of given level.
  * It calculates the size of the sponge, iterates over each position, and
- * determines whether it should be empty or filled by calling the is_empty function.
+ * determines whether it should be empty or filled by calling
+ * the is_empty function.
  */
 void menger(int level)
 {

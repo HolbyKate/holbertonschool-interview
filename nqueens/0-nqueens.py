1fc924f33518ed7f1c95b7380b9f5c063659dd11
HolbyKate <cathy.augustin@holbertonstudents.com>
2024-07-29 17:05:37 +0200

    Test the left side

--- nqueens/0-nqueens.py
+++ nqueens/0-nqueens.py
@@ -1,2 +1,13 @@
 #!/usr/bin/python3
 
+import sys
+
+def is_safe(board, row, col, n):
+    # Check this row on left side
+    for i in range(col):
+        if board[row][i] == 1:
+            return False
+
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False

77abf85c380f9053d10557fd6bdb576af1d7ce31
HolbyKate <cathy.augustin@holbertonstudents.com>
2024-08-02 21:43:14 +0200

    found  # If all columns have been processed

--- nqueens/0-nqueens.py
+++ nqueens/0-nqueens.py
@@ -20,3 +20,47 @@ def is_safe(board, row, col, n):
             return False
 
     return True
+
+def solve_nqueens(n):
+    board = [[0 for _ in range(n)] for _ in range(n)]
+    solutions = []
+
+    def solve(col):
+        if col >= n:
+            solution = []
+            for i in range(n):
+                for j in range(n):
+                    if board[i][j] == 1:
+                        solution.append([i, j])
+            solutions.append(solution)
+            return True
+
+        res = False
+        for i in range(n):
+            if is_safe(board, i, col, n):
+                board[i][col] = 1
+                res = solve(col + 1) or res
+                board[i][col] = 0
+        return res
+
+    solve(0)
+    return solutions
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    try:
+        n = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    solutions = solve_nqueens(n)
+    for solution in solutions:
+        print(solution)

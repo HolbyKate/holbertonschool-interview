65f229a2936e53f8e8507d56bdd55d0eea56313d
HolbyKate <cathy.augustin@holbertonstudents.com>
2024-08-02 21:33:42 +0200

    update pep8

--- nqueens/0-nqueens.py
+++ nqueens/0-nqueens.py
@@ -9,7 +9,7 @@ def is_safe(board, row, col, n):
         if board[row][i] == 1:
             return False
 
-    #Check upper diagonal on left side
+    # Check upper diagonal on left side
     for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
         if board[i][j] == 1:
             return False

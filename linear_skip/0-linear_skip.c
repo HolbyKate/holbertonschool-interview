e134895c30d82c4e07047ca76945dffb00986b06
HolbyKate <cathy.augustin@holbertonstudents.com>
2024-08-16 14:55:01 +0200

    Implement linear_skip function for searching in skip lists

---
+++ linear_skip/0-linear_skip.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "search.h"
+
+/**
+ * linear_skip - checks if a singly linked list has a cycle
+ * @list: Pointer to the head of the skip list to search in
+ * @value: the value to search for
+ *
+ * Return: pointer on the first node where value is located
+ */
+
+skiplist_t *linear_skip(skiplist_t *list, int value)
+{
+    skiplist_t *express, *prev;
+}

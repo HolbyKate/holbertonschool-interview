924d988f59f6dc07909c29d95cc62540cab81ca8
HolbyKate <cathy.augustin@holbertonstudents.com>
2024-08-23 15:01:40 +0200

    Fix: adding space before () function

--- starwars_api/0-starwars_characters.js
+++ starwars_api/0-starwars_characters.js
@@ -3,7 +3,7 @@
 const request = require('request');
 
 // Fetches a character's name from the given URL.
-function fetchCharacter(url) {
+function fetchCharacter (url) {
     return new Promise((resolve, reject) => {
         request(url, (error, response, body) => {
             if (error) {
@@ -15,7 +15,8 @@ function fetchCharacter(url) {
 }
 
 // Retrieves and prints the names of all characters in a Star Wars movie.
-async function getCharacterNames(movieId) {
+
+async function getCharacterNames (movieId) {
     const movieUrl = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
 
     request(movieUrl, async (error, response, body) => {

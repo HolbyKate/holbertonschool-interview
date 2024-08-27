d2cac2182801063ac9fb79e6466095fab37878f0
HolbyKate <cathy.augustin@holbertonstudents.com>
2024-08-27 11:55:09 +0200

    Initialize pointer and left max

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -5,3 +5,11 @@ def rain(walls):
     # If list is empty, return 0
     if not walls:
         return 0
+
+    left = 0
+    right = len(walls) - 1
+    left_max = right_max = water = 0
+
+    while left < right:
+        if walls[left] > left_max:
+            left_max = walls[left]

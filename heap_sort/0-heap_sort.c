2b7f45d0c7b5adc7fc9d7614e2dde49dad9724eb
HolbyKate <cathy.augustin@holbertonstudents.com>
2024-09-13 14:48:14 +0200

    Improve heap_sort algorithm implementation

--- heap_sort/0-heap_sort.c
+++ heap_sort/0-heap_sort.c
@@ -1,69 +1,66 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "sort.h"
+#include <stdio.h> /* For printf */
 
 /**
- * swap - Swap two elements in an array
+ * swap - Swaps two elements in an array.
  *
- * @array: The array to be sorted
- * @i: The index of the first element
- * @j: The index of the second element
+ * @a: First element.
+ * @b: Second element.
  */
-void swap(int *array, size_t i, size_t j)
+void swap(int *a, int *b)
 {
-	int temp = array[i];
-	array[i] = array[j];
-	array[j] = temp;
+	int temp = *a;
+	*a = *b;
+	*b = temp;
 }
 
 /**
- * sift_down - Sift down the element at index i in a max heap
+ * sift_down - Maintains the max heap property.
  *
- * @array: The array representing the heap
- * @size: Total size of the array
- * @n: Size of the heap portion of the array
- * @i: The index of the element to sift down
+ * @array: The array to be sorted.
+ * @size: Total size of the array.
+ * @root: Root index of the current subtree.
+ * @n: Size of the heap to consider (may shrink over time).
  */
-void sift_down(int *array, size_t size, size_t n, size_t i)
+void sift_down(int *array, size_t size, size_t root, size_t n)
 {
-	size_t largest = i;
-	size_t left = 2 * i + 1;
-	size_t right = 2 * i + 2;
+	size_t largest = root;
+	size_t left = 2 * root + 1;
+	size_t right = 2 * root + 2;
 
 	if (left < n && array[left] > array[largest])
 		largest = left;
-
 	if (right < n && array[right] > array[largest])
 		largest = right;
 
-	if (largest != i)
+	if (largest != root)
 	{
-		swap(array, i, largest);
-		print_array(array, size);
-
-		sift_down(array, size, n, largest);
+		swap(&array[root], &array[largest]);
+		print_array(array, size); /* Print after each swap */
+		sift_down(array, size, largest, n);
 	}
 }
 
 /**
- * heap_sort - Sort an array of integers in ascending order using Heap sort
+ * heap_sort - Sorts array of integers in asce order using Heap sort algorithm.
  *
- * @array: The array to be sorted
- * @size: Size of the array
+ * @array: The array to be sorted.
+ * @size: The size of the array.
  */
 void heap_sort(int *array, size_t size)
 {
-	if (array == NULL || size < 2)
+	if (size < 2)
 		return;
 
-	for (size_t i = (size / 2) - 1; i >= 0; i--)
-		sift_down(array, size, size, i);
+	/* Build the max heap */
+	for (int i = (size / 2) - 1; i >= 0; i--)
+		sift_down(array, size, i, size);
 
-	for (size_t i = size - 1; i > 0; i--)
+	/* Extract elements from the heap */
+	for (int i = size - 1; i > 0; i--)
 	{
-		swap(array, 0, i);
-		print_array(array, size);
-
-		sift_down(array, size, i, 0);
+		swap(&array[0], &array[i]);
+		print_array(array, size); /* Print after each swap */
+		sift_down(array, size, 0, i);
 	}
 }

77786b3f5080d0ec01e807d82d6d332a3406bdb4
HolbyKate <6804@holbertonstudents.com>
2024-10-02 09:04:09 +0200

    Create files and prototype

---
+++ infinite_multiplication/holberton.h
@@ -0,0 +1,11 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+int _putchar(char c);
+void _print_error(void);
+int _isdigit(char *s);
+void multiply(char *num1, char *num2);
+int _strlen(char *s);
+void _puts(char *s);
+
+#endif /* _HOLBERTON_H_ */

5d9a5b490a76aea90ae51ad6a9ad5dcb4b1f0108
HolbyKate <6804@holbertonstudents.com>
2024-10-03 11:07:06 +0200

    Fix: Delete putchar

--- infinite_multiplication/0-mul.c
+++ infinite_multiplication/0-mul.c
@@ -3,15 +3,6 @@
 #include <stdio.h>
 #include <unistd.h>
 
-/**
- * _putchar - Writes a character to stdout.
- * @c: The character to print.
- * Return: On success 1, on error -1.
- */
-int _putchar(char c)
-{
-	return write(1, &c, 1);
-}
 
 /**
  * _print_error - Prints error and exits.

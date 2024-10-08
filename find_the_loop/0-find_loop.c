e48b2ff6440dda46c0d57698fdaec88d00390d59
HolbyKate <6804@holbertonstudents.com>
2024-10-08 14:18:53 +0200

    Work with only pointers

--- find_the_loop/0-find_loop.c
+++ find_the_loop/0-find_loop.c
@@ -1,129 +1,36 @@
 #include "lists.h"
-#include <stdlib.h>
-#include <unistd.h>
 
 /**
- * add_nodeint - Adds a new node at the beginning of a listint_t list.
- * @head: Double pointer to the head of the list.
- * @n: Integer value for the new node.
+ * find_listint_loop - Finds the loop in a linked list
+ * @head: Pointer to the head of the list
  *
- * Return: Address of the new element, or NULL if it failed.
- */
-
-void custom_putc(char c)
-{
-    write(1, &c, 1);
-}
-
-void custom_print(int n)
-{
-    char buffer[12];
-    int i = 0, j, digit;
-
-    if (n < 0)
-    {
-        n = -n;
-        buffer[i++] = '-';
-    }
-
-    do
-    {
-        digit = n % 10;
-        buffer[i++] = digit + '0';
-        n /= 10;
-    } while (n > 0);
-
-    for (j = (buffer[0] == '-' ? 1 : 0); j < i; j++)
-        custom_putc(buffer[i - j - 1]);
-    custom_putc('\n');
-}
-
-listint_t *add_nodeint(listint_t **head, const int n)
-{
-    listint_t *new_node;
-
-    new_node = (listint_t *)malloc(sizeof(listint_t));
-    if (!new_node)
-        return (NULL);
-
-    new_node->n = n;
-    new_node->next = *head;
-    *head = new_node;
-
-    return (new_node);
-}
-
-/**
- * print_listint_safe - Prints a listint_t list safely.
- * @head: Pointer to the head of the list.
- *
- * Return: The number of nodes in the list.
- */
-size_t print_listint_safe(const listint_t *head)
-{
-    const listint_t *current = head;
-    size_t count = 0;
-
-    while (current)
-    {
-        custom_print(current->n);
-        count++;
-        current = current->next;
-    }
-    return (count);
-}
-
-/**
- * free_listint_safe - Frees a listint_t list safely.
- * @h: Double pointer to the head of the list.
- *
- * Return: The size of the list that was freed.
- */
-size_t free_listint_safe(listint_t **h)
-{
-    size_t count = 0;
-    listint_t *temp;
-
-    while (*h)
-    {
-        temp = *h;
-        *h = (*h)->next;
-        free(temp);
-        count++;
-    }
-    return (count);
-}
-
-/**
- * find_listint_loop - Finds the loop in a linked list.
- * @head: Pointer to the head of the linked list.
- *
- * Return: The address of the node where the loop starts, or NULL if no loop.
+ * Return: The address of the node where the loop starts, or NULL if no loop
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-    listint_t *tortoise = head;
-    listint_t *hare = head;
+	listint_t *tortoise, *hare;
 
-    while (hare != NULL && hare->next != NULL)
+	if (head == NULL || head->next == NULL)
+		return (NULL);
 
-    {
-        tortoise = tortoise->next;
-        hare = hare->next->next;
+	tortoise = head->next;
+	hare = head->next->next;
 
-        if (tortoise == hare)
-            break;
-    }
+	while (hare && hare->next)
+	{
+		if (tortoise == hare)
+		{
+			tortoise = head;
+			while (tortoise != hare)
+			{
+				tortoise = tortoise->next;
+				hare = hare->next;
+			}
+			return (tortoise);
+		}
+		tortoise = tortoise->next;
+		hare = hare->next->next;
+	}
 
-    if (hare == NULL || hare->next == NULL)
-        return (NULL);
-
-    tortoise = head;
-    while (tortoise != hare)
-    {
-        tortoise = tortoise->next;
-        hare = hare->next;
-    }
-
-    return (tortoise);
+	return (NULL);
 }

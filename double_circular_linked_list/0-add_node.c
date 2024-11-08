0392d30c5aa55fa6b2859a8a508d6e2db4972e40
HolbyKate <6804@holbertonstudents.com>
2024-11-08 09:31:43 +0100

    First attempt for project

---
+++ double_circular_linked_list/0-add_node.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <string.h>
+#include "list.h"
+
+/**
+* add_node_end - adding a new node at the end of the doble linked list
+* @list: pointer to head list
+* @str: string will be adding in the new node
+*
+* return: the adress of the new node or NULL if is failing
+*/
+

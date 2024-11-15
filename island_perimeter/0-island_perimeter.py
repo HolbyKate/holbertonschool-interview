0eb2681d41a8467125ffc3352680489dc4da2802
HolbyKate <6804@holbertonstudents.com>
2024-11-15 22:12:30 +0100

    First attempt of perimeter

---
+++ island_perimeter/0-island_perimeter.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+def island_perimeter(grid):
+    """
+        Calculate the perimeter of the island described in grid.
+
+        Args:
+            grid (List[List[int]]): A 2D grid where:
+                - 0 represents water
+                - 1 represents land
+
+        Returns:
+            int: The perimeter of the island
+        """
+
+    if not grid:
+        return 0
+
+    perimeter = 0
+    rows = len(grid)
+    cols = len(grid[0])
+    # define parameter
+    for i in range(rows):
+        if grid[i] == 1:
+            perimeter += 4
+
+    for j in range(cols):
+        if grid[j] == 1:
+            perimeter += 4
+            # Check cell on top
+            if i > 0 and grid[i-1][j] == 1:
+                perimeter -= 2
+
+            # Check cell to the left
+            if j > 0 and grid[i][j-1] == 1:
+                perimeter -= 2
+    return perimeter

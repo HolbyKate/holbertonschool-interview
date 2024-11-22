d64efd846af3d6977283eda205e2d0549454c247
HolbyKate <6804@holbertonstudents.com>
2024-11-22 10:23:04 +0100

    Adding binary_tree_node to make nodes

--- avl_trees/0-binary_tree_is_avl.c
+++ avl_trees/0-binary_tree_is_avl.c
@@ -1,16 +1,17 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "binary_trees.h"
+#include <limits.h>
 
 /**
  * binary_tree_is_avl - checks if a binary tree is a valid AVL tree
- * @tree: Pointer to the root node of the tree to check
+ * @tree: Pointer to the root node of the tree
  *
- * Return: 1 if tree is a valid AVL tree, 0 otherwise
+ * Return: height of the tree, 0 otherwise
  */
 int binary_tree_is_avl(const binary_tree_t *tree)
 {
-    if (!tree)
+    if (tree == NULL)
         return (0);
     return (binary_tree_is_avl(tree->left) && binary_tree_is_avl(tree->right));
 }

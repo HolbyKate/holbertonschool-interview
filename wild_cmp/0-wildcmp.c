9a56f8ec966eb831c0a791aff5b8b1b76d432017
HolbyKate <6804@holbertonstudents.com>
2024-11-29 22:44:07 +0100

    gg=G

--- wild_cmp/0-wildcmp.c
+++ wild_cmp/0-wildcmp.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
-* wildcmp - Compares two strings
+ * wildcmp - Compares two strings
  * @s1: string 1
  * @s2: string 2 with wildcards
  *
@@ -11,28 +11,28 @@
 
 int wildcmp(char *s1, char *s2)
 {
-    // If empty it match
-    if (*s1 == '\0' && *s2 == '\0')
-        return (1);
+	// If empty it match
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
 
-    // If we find a '*' in s2
-    if (*s2 == '*')
-    {
-        if (*(s2 + 1) == '*')
-            return (wildcmp(s1, s2 + 1));
+	// If we find a '*' in s2
+	if (*s2 == '*')
+	{
+		if (*(s2 + 1) == '*')
+			return (wildcmp(s1, s2 + 1));
 
-        // 1. '*' matches empty string: skip '*' in s2
-        if (wildcmp(s1, s2 + 1))
-            return (1);
-        // 2. '*' matches current char in s1: keep '*' and move s1
-        if (*s1 != '\0' && wildcmp(s1 + 1, s2))
-            return (1);
-        return (0);
-    }
+		// 1. '*' matches empty string: skip '*' in s2
+		if (wildcmp(s1, s2 + 1))
+			return (1);
+		// 2. '*' matches current char in s1: keep '*' and move s1
+		if (*s1 != '\0' && wildcmp(s1 + 1, s2))
+			return (1);
+		return (0);
+	}
 
-    // If current characters match, go to next characters
-    if (*s1 == *s2)
-        return (wildcmp(s1 + 1, s2 + 1));
+	// If current characters match, go to next characters
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
 
-    return (0);
+	return (0);
 }

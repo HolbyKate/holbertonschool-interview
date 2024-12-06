09c496696c37731caa2b61162d421dca6bf48447
HolbyKate <6804@holbertonstudents.com>
2024-12-06 21:45:10 +0100

    Adding new lines in function to create calcul of Pascal's Triangle

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -9,10 +9,20 @@ def pascal_triangle(n):
     if n <= 0:
         return []
 
-    triangle = [1]
+    triangle = [[1]]
 
     # Create each line
     for i in range(1, n):
         prev_row = triangle[-1]
         # Start with new line
         new_row = [1]
+
+        # Then with middle line
+        for j in range(len(prev_row) - 1):
+            new_row.append(prev_row[j] + prev_row[j + 1])
+
+        # Finish with last line
+        new_row.append(1)
+        triangle.append(new_row)
+
+    return triangle
